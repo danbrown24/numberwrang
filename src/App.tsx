f9a99eb0ec7113d494318ccd7f23ae94659a1e0b
Dan Brown <danbrown24@gmail.com>
2024-02-26 14:44:28 +0000

    Add code to offer a dark mode change callback externally

--- src/App.tsx
+++ src/App.tsx
@@ -8,11 +8,17 @@ import Instructions from './components/Instructions';
 import { setBodyClass } from './lib';
 import Game from './components/Game';
 import Win from './components/Win';
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
+
+type DarkModeChangeListener = (darkMode: boolean) => void
 
 declare global {
   // eslint-disable-next-line no-var
   var _setAppDarkMode: (darkMode: boolean) => void;
+  // eslint-disable-next-line no-var
+  var _registerDarkModeChangeListener: (cb: DarkModeChangeListener) => void;
+  // eslint-disable-next-line no-var
+  var flutter_inappwebview: undefined | { callHandler: (name: string, ...args: any[]) => void };
 }
 
 initFirebase();
@@ -33,11 +39,23 @@ const AppInner = () => {
 };
 
 const AppWrapper = () => {
+  const darkModeChangeListenersRef = useRef<DarkModeChangeListener[]>([]);
   const [darkMode, setDarkMode] = useRecoilState(darkModeState);
 
   useEffect(() => {
     globalThis._setAppDarkMode = (darkMode: boolean) => setDarkMode(!!darkMode);
+    globalThis._registerDarkModeChangeListener = (cb: DarkModeChangeListener) => {
+      if (darkModeChangeListenersRef.current.length > 10) {
+        throw new Error('Cannot register any more dark mode listeners');
+      }
+      darkModeChangeListenersRef.current.push(cb);
+    };
   }, [setDarkMode]);
+  
+  useEffect(() => {
+    darkModeChangeListenersRef.current.forEach((cb) => cb(darkMode));
+    window.flutter_inappwebview?.callHandler('darkModeChangeHandler', darkMode); // Custom callback if we're embedded in a Flutter webview
+  }, [darkMode]);
 
   return (
     <div className={classNames('app', { 'dark-mode': darkMode })} data-testid="app-main">
